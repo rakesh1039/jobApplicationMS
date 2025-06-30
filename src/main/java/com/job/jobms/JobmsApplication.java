078de7866c31c52964cdd0f8d165335df097caf4
rakesh1039 <97017149+rakesh1039@users.noreply.github.com>
2025-06-30 21:40:08 +0530

    Initial commit

---
+++ src/main/java/com/job/jobms/JobmsApplication.java
@@ -0,0 +1,12 @@
+package com.job.jobms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobmsApplication.class, args);
+	}
+}
